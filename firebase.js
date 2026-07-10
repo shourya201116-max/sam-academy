028ce7b31e4a4d27582125699880b6f236e9c294
shourya201116-max <shourya201116@gmail.com>
2026-07-10 23:15:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Replace these values with your Firebase project's configuration
 const firebaseConfig = {
-  apiKey: "",// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBOdaiU8udAXB4r0l61s2gY7Sg8tzUYSCE",
+  apiKey: "AIzaSy...",
   authDomain: "sam-academy-47081.firebaseapp.com",
   projectId: "sam-academy-47081",
   storageBucket: "sam-academy-47081.firebasestorage.app",
