fbcebb20a88147565dcb5bf343ad2d49a5985e4d
shourya201116-max <shourya201116@gmail.com>
2026-07-10 23:11:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 
 // Replace these values with your Firebase project's configuration
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "",// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOdaiU8udAXB4r0l61s2gY7Sg8tzUYSCE",
+  authDomain: "sam-academy-47081.firebaseapp.com",
+  projectId: "sam-academy-47081",
+  storageBucket: "sam-academy-47081.firebasestorage.app",
+  messagingSenderId: "280246545606",
+  appId: "1:280246545606:web:476e95e1ceb9e5bba37bc7",
+  measurementId: "G-20QVRYFWBC"
+};
   authDomain: "",
   projectId: "",
   storageBucket: "",
