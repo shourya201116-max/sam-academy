332ff8b82e12c5516aa7f925716b3ed3af6d0e22
shourya201116-max <shourya201116@gmail.com>
2026-07-10 22:57:13 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Replace these values with your Firebase project's configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
