7b35bbb692de95218b8deaa9ece58435d08f83d8
shourya201116-max <shourya201116@gmail.com>
2026-07-11 12:02:05 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
