b349f63aa307acc2852f13daa0451a1b48c62179
shourya201116-max <shourya201116@gmail.com>
2026-07-11 15:43:47 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,78 +1,60 @@
 import { db } from "./firebase.js";
 
 import {
-collection,
-addDoc,
-getDocs
+  collection,
+  addDoc,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const table = document.getElementById("studentTable");
+
 // Load all students
-async function loadStudents(){
+async function loadStudents() {
 
-const table=document.getElementById("studentTable");
+  table.innerHTML = "";
 
-table.innerHTML="";
+  const querySnapshot = await getDocs(collection(db, "students"));
 
-const querySnapshot=await getDocs(collection(db,"students"));
+  querySnapshot.forEach((doc) => {
 
-querySnapshot.forEach((doc)=>{
+    const student = doc.data();
 
-const student=doc.data();
+    table.innerHTML += `
+      <tr>
+        <td>${student.name}</td>
+        <td>${student.class}</td>
+        <td>${student.attendance}%</td>
+        <td>${student.marks}%</td>
+        <td>${student.fees}</td>
+        <td>
+          <button>Edit</button>
+          <button>Delete</button>
+        </td>
+      </tr>
+    `;
 
-table.innerHTML+=`
-<tr>
-<td>${student.name}</td>
-<td>${student.class}</td>
-<td>${student.attendance}%</td>
-<td>${student.marks}%</td>
-<td>${student.fees}</td>
-<td>
-<button>Edit</button>
-<button>Delete</button>
-</td>
-</tr>
-`;
-
-});
+  });
 
 }
 
 // Add student
-const addButton=document.querySelector(".add-btn");
+document.querySelector(".add-btn").addEventListener("click", async () => {
 
-addButton.addEventListener("click",async()=>{
+  const inputs = document.querySelectorAll(".section input");
+  const fees = document.querySelector("select");
 
-const inputs=document.querySelectorAll(".section input");
+  await addDoc(collection(db, "students"), {
+    name: inputs[0].value,
+    class: inputs[1].value,
+    attendance: Number(inputs[2].value),
+    marks: Number(inputs[3].value),
+    fees: fees.value
+  });
 
-const fees=document.querySelector("select");
+  alert("Student Added Successfully!");
 
-try{
-
-await addDoc(collection(db,"students"),{
-
-name:inputs[0].value,
-
-class:inputs[1].value,
-
-attendance:Number(inputs[2].value),
-
-marks:Number(inputs[3].value),
-
-fees:fees.value
+  loadStudents();
 
 });
 
-alert("Student Added Successfully!");
-
-loadStudents();
-
-}catch(error){
-
-alert(error.message);
-
-}
-
-});
-
-// First load
 loadStudents();
